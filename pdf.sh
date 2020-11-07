f07cf04d7564c6675d3feab68f81d7ecc73e78d9
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-11-07 10:44:35 +0900

    add build script for pdf.

---
+++ pdf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+DOCKER_HOME=$SCRIPT_DIR/container
+TARGET_HOME=/home/yocto
+
+mkdir -p $DOCKER_HOME
+docker run -u yocto:yocto -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-manual:latest ./build.sh
+
