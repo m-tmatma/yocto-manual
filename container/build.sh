fb508c8893c2649fdb913d02fefffa76a41256e2
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-11-07 17:08:45 +0900

    add other doc build.

--- container/build.sh
+++ container/build.sh
@@ -2,5 +2,13 @@
 
 cd $(dirname $0)
 cd poky/documentation
+
+make pdf DOC=sdk-manual
+make pdf DOC=bsp-guide
+make pdf DOC=dev-manual
+make pdf DOC=kernel-dev
 make pdf DOC=ref-manual
+make pdf DOC=yocto-project-qs
+make pdf DOC=profile-manual
+make pdf DOC=toaster-manual
 
