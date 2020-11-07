d5f44363dd0e32ed1aa85d961ed9a2f263ed75fd
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-11-07 13:04:55 +0900

    change remote url.

--- clone.sh
+++ clone.sh
@@ -8,7 +8,8 @@ DOCKER_HOME=$SCRIPT_DIR/container
 GIT_DIR=${DOCKER_HOME}/poky
 
 if [ ! -e ${GIT_DIR} ]; then
-	git clone https://git.yoctoproject.org/git/poky ${GIT_DIR}
+	#git clone https://git.yoctoproject.org/git/poky ${GIT_DIR}
+	git clone git://git.yoctoproject.org/poky ${GIT_DIR}
 else
 	echo ${GIT_DIR} exists: skip
 fi
