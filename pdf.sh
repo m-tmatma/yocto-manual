77c0f689abb59d7e995779902a903a1b8a4a1a21
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-06-26 20:51:56 +0900

    use common variable

--- pdf.sh
+++ pdf.sh
@@ -3,7 +3,8 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 DOCKER_HOME=$SCRIPT_DIR/container
 TARGET_HOME=/home/yocto
+. $SCRIPT_DIR/common.sh
 
 mkdir -p $DOCKER_HOME
-docker run -u yocto:yocto -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-manual:latest ./build.sh
+docker run -u yocto:yocto -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME $DOCKER_IMAGE ./build.sh
 
